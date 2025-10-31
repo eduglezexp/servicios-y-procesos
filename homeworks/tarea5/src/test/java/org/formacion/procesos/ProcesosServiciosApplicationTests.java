967506ba927b05607c6a91f7da981b7b231c95a5
eduglezexp <eduex04@gmail.com>
2025-10-31 20:29:52 +0000

    updating pom

--- homeworks/tarea5/src/test/java/org/formacion/procesos/ProcesosServiciosApplicationTests.java
+++ homeworks/tarea5/src/test/java/org/formacion/procesos/ProcesosServiciosApplicationTests.java
@@ -1,11 +1,7 @@
 package org.formacion.procesos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ProcesosServiciosApplicationTests {
-    @Test
+
     void contextLoads() {
     }
 }
