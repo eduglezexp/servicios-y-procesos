f4d3c98edd67f03a187b012a1e8e908b5a06d8d6
Eduardo González <eduex04@gmail.com>
2025-11-15 22:17:43 +0000

    actividad2 finished

---
+++ homeworks/3-multihilo/tarea2/src/main/java/com/docencia/semaforo/ejercicio2/Estudiante.java
@@ -0,0 +1,52 @@
+package com.docencia.semaforo.ejercicio2;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author eduglezexp
+ * @version 1.0.0
+ */
+
+public class Estudiante implements Runnable {
+
+    private final String nombre;
+    private static final Semaphore semaforo = new Semaphore(4);
+    
+    /**
+     * Constructor por defecto
+     */
+    public Estudiante() {
+        nombre = "bot";
+    }
+
+    /**
+     * Constructor con la propiedad nombre
+     * @param nombre del estudiante
+     */
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            semaforo.acquire();
+            System.out.println(nombre + " ha comenzado a utilizar el equipo.");
+            int tiempo = ThreadLocalRandom.current().nextInt(3, 6) * 1000;
+            Thread.sleep(tiempo);
+            System.out.println(nombre + " ha finalizado con el equipo.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            semaforo.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            Thread estudiante = new Thread(new Estudiante("Estudiante " + i));
+            estudiante.start();
+        }
+    }
+}
---
+++ homeworks/3-multihilo/tarea2/src/main/java/com/docencia/semaforo/ejercicio2/EstudianteMejorado.java
@@ -0,0 +1,53 @@
+package com.docencia.semaforo.ejercicio2;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author eduglezexp
+ * @version 1.0.0
+ */
+
+public class EstudianteMejorado implements Runnable {
+
+    private final String nombre;
+    private static final Semaphore semaforo = new Semaphore(4);
+    
+    /**
+     * Constructor por defecto
+     */
+    public EstudianteMejorado() {
+        nombre = "bot";
+    }
+
+    /**
+     * Constructor con la propiedad nombre
+     * @param nombre del estudiante
+     */
+    public EstudianteMejorado(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            semaforo.acquire();
+            int equipo = semaforo.availablePermits() + 1;
+            System.out.println(nombre + " ha comenzado a utilizar el equipo " + equipo);
+            int tiempo = ThreadLocalRandom.current().nextInt(3, 6) * 1000;
+            Thread.sleep(tiempo);
+            System.out.println(nombre + " ha finalizado con el equipo " + equipo);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            semaforo.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            Thread estudiante = new Thread(new EstudianteMejorado("Estudiante " + i));
+            estudiante.start();
+        }
+    }
+}
